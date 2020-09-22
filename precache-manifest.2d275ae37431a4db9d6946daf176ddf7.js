e4d1aeb5c1d99cf84e37f039b9b0f12232b49d34
mugymb <Mr.Mb@tuta.io>
2020-09-22 19:57:21 +0500

    Updates

---
+++ precache-manifest.2d275ae37431a4db9d6946daf176ddf7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a20e442be8bf4316ae52dac3895d79",
+    "url": "/developerIdentity/index.html"
+  },
+  {
+    "revision": "c69306269d006a38d294",
+    "url": "/developerIdentity/static/css/2.5290c630.chunk.css"
+  },
+  {
+    "revision": "897ddda688ed344915f4",
+    "url": "/developerIdentity/static/css/main.ae7c265c.chunk.css"
+  },
+  {
+    "revision": "c69306269d006a38d294",
+    "url": "/developerIdentity/static/js/2.aed95f2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/developerIdentity/static/js/2.aed95f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "897ddda688ed344915f4",
+    "url": "/developerIdentity/static/js/main.0c0d037a.chunk.js"
+  },
+  {
+    "revision": "32021aaa735800edbe6a",
+    "url": "/developerIdentity/static/js/runtime-main.4576985b.js"
+  },
+  {
+    "revision": "5504b8112117e260221ae1f4226d68a9",
+    "url": "/developerIdentity/static/media/a.5504b811.svg"
+  },
+  {
+    "revision": "ab129dd2f0a3e621dd6a6d82eca69ee2",
+    "url": "/developerIdentity/static/media/abot.ab129dd2.svg"
+  },
+  {
+    "revision": "7dfac08d683b81d4755c4f28ae936f2e",
+    "url": "/developerIdentity/static/media/bg.7dfac08d.jpg"
+  },
+  {
+    "revision": "a640949a46510ce0de48b8f8757ba3d3",
+    "url": "/developerIdentity/static/media/df4.a640949a.svg"
+  },
+  {
+    "revision": "f24f6e014f9849a9f8147bbbc71dedd9",
+    "url": "/developerIdentity/static/media/e.f24f6e01.svg"
+  },
+  {
+    "revision": "fb5ec271f3c8677751313a157d4162a0",
+    "url": "/developerIdentity/static/media/fot.fb5ec271.svg"
+  },
+  {
+    "revision": "3227854943a224f307bd289dd284d52d",
+    "url": "/developerIdentity/static/media/log.32278549.svg"
+  },
+  {
+    "revision": "b5012fa9723f3e06ab13841e7ed4ad48",
+    "url": "/developerIdentity/static/media/m1.b5012fa9.svg"
+  },
+  {
+    "revision": "c34dcd63e13ffb060cb210bf83cc8f87",
+    "url": "/developerIdentity/static/media/mb.c34dcd63.svg"
+  }
+]);
